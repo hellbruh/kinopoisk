81e683b14a189492417f544ce587213aafd3ee61
Annya <annya@example.com>
2023-02-18 19:03:49 +0500

    поворкали в академи 18

--- FilmPage.js
+++ FilmPage.js
@@ -8,9 +8,13 @@ let filmGenresId = document.querySelector('#filmGenres')
 let filmLengthId = document.querySelector('#filmLength')
 let filmRatingId1 = document.querySelector('#filmRating1')
 let filmRatingId2 = document.querySelector('#filmRating2')
-let commentText = document.querySelector('#commentText')
-
 let commentTextId = document.querySelector('#commentText')
+let commentsId = document.querySelector('.otherComments')
+
+//заранее очистили поле для комма после обновления стр
+commentTextId.value = ''
+
+
 // получаем айдишник фильма из query parameter
 let filmID = window.location.href
 filmID = filmID.split('=')
@@ -39,12 +43,7 @@ async function getFilm(filmId){
     }
 }
 getFilm(filmID)
-
-    //todo: добавить больше информации.
-    //Давай название на русском/на английском
-    //Рейтинг кинопоиска
-    //Год выпуска, продолжительность фильма, 
-    // слоган, страна выпуска, жанр
+getComments(filmID)
 
 function showFilm(film){ 
     let filmName = film.nameRu || film.nameOriginal
@@ -115,5 +114,77 @@ function showFilm(film){
         }
     }
     filmGenresId.innerHTML += genres
+}
+
+let comments = []
+
+async function getComments(filmId){
+    url=`http://192.168.1.21:5000/comments/show/${filmId}`
+    try{
+        let response =await axios.get (url)
+        console.log(response)
+        comments = response.data.message
+        showComments()
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+
+let rating = []
+function leaveRating(n){
+    rating.push(n)
+    for (let i=1; i<11;i++){
+        let st = document.getElementById(`rating-${i}`)
+        st.style.color = 'rgb(109, 109, 109)'
+    }
+    let s = document.getElementById(`rating-${n}`)
+    console.log(n)
+    s.style.color = 'white'
+    console.log(rating)
+}
+
+async function postComment(){
+    let commentText = commentTextId.value
+    let commentRating = rating[rating.length-1]
+    let comment = {
+        filmId: filmID,
+        filmRating:commentRating,
+        commentText:commentText,
+        userId:467 
+    }
+    console.log('sent')
+    console.log(comment)
+    commentTextId.value = ''
+    
+    // postim comment
+    url = `http://192.168.1.21:5000/comments/add`
+    try{
+        let response = await axios.post (url,comment)
+        console.log(response.data)
+        comments.push(response.data.message)
+    }
+    catch(error){
+        console.error(error)
+    }
+    
+    showComments()
+    
+}
+
+function showComments(){
+    if (comments.length ==0){
+        commentsId.innerHTML = `There're no comments yet`
+    }
+    else{
+        commentsId.innerHTML=``
+        for (let com of comments){
+            t = `
+            <div>${com.fio}</div>
+            <div>${com.filmRating}</div>
+            <div>${com.commentText}</div>`
+            commentsId.innerHTML+=t
+        }
+    }
 }
 
