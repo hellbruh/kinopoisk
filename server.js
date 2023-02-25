f9cc9ee49f0ff90b87509c2dc4ccd2ffbb4c4b58
Annya <annya@example.com>
2023-02-25 18:31:55 +0500

    dodelaliiiiiiiii 25.02

---
+++ server.js
@@ -0,0 +1,11 @@
+const fastify = require('fastify')({
+    logger: true
+  })
+ 
+
+fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+  })
